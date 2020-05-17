f556316039985e3f1500536fba0a8261a85f54d1
lah5175 <lah5175@hotmail.com>
2020-05-16 20:45:45 -0400

    Added basic login/signup forms

---
+++ client/components/AuthForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AuthForm = props => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" value={props.username} onChange={props.handleChange} />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" value={props.email} onChange={props.handleChange} />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" value={props.password} onChange={props.handleChange} />
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default AuthForm;
---
+++ client/components/Signup.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import AuthForm from './AuthForm';
+
+class Signup extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      email: '',
+      password: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      username: '',
+      email: '',
+      password: ''
+    })
+  }
+
+  render() {
+    return (
+      <AuthForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} {...this.state} />
+    )
+  }
+}
+
+export default Signup;
